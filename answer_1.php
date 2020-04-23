8cec01ba2d64b25db9126d35d967cf7a0f77756b
unknown <pelolong24@gmail.com>
2020-04-23 19:04:44 +0700

    answer_1

---
+++ answer_1.php
@@ -0,0 +1,19 @@
+<?php
+
+$angka=8; //variable awal adalah 8
+
+echo "nilai angka= $angka";
+echo "<br>";
+echo $angka+2;
+echo "<br>";
+echo $angka-2;
+echo "<br>";
+echo $angka+22;
+echo "<br>";
+echo $angka+2;
+echo "<br>";
+echo $angka+3;
+echo "<br>";
+echo $angka+2;
+
+?>
